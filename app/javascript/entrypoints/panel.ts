040c5a0ca46cc344fb79570c064c94019ab1cf81
hama <hmmshl@gmail.com>
2024-03-07 10:10:33 +0100

    panel css added

--- app/javascript/entrypoints/panel.ts
+++ app/javascript/entrypoints/panel.ts
@@ -2,6 +2,7 @@
 import {createApp} from "vue";
 import App from '@/layouts/panel/MainLayout.vue';
 import {registerPlugins} from "@/plugins/index.js";
+import '@/assets/css/panel.scss';
 
 const app = createApp(App);
 
