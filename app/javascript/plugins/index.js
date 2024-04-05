35818e09fbe0c8c094c1e0d8e9b6543192dda2c4
hama <hmmshl@gmail.com>
2024-04-05 09:22:17 +0200

    bootstrap removed

--- app/javascript/plugins/index.js
+++ app/javascript/plugins/index.js
@@ -5,13 +5,9 @@ import AuthService from '@/services/auth.service';
 import i18n from "@/plugins/i18n.js";
 import router from "@/routes/website.js";
 import { showToast } from '@/utils/showToast';
-// import BootstrapVue from 'bootstrap-vue'; // Uncomment this line
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import jQuery from 'jquery'
 
 window.$ = window.jQuery = jQuery;
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 export const globalProperties = {
   $showToast: showToast,
 };
@@ -30,10 +26,5 @@ export const registerPlugins = (app) => {
   app.use(pinia);
   app.use(VueQueryPlugin);
   app.use(i18n);
-  // app.use(BootstrapVue)
-  // Make BootstrapVue available throughout your project
-  // app.use(BootstrapVue)
-  // Optionally install the BootstrapVue icon components plugin
-  // app.use(IconsPlugin)
   app.config.globalProperties = { ...app.config.globalProperties, ...globalProperties };
 };
