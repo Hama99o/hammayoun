5bafbed5583f95d54ed2750efa7cd2e59598e8d7
hama <hmmshl@gmail.com>
2024-04-07 22:36:25 +0200

    tableimproved

--- app/javascript/components/users/Table.vue
+++ app/javascript/components/users/Table.vue
@@ -2,25 +2,56 @@
   <v-table>
     <thead>
       <tr>
-        <th class="text-left">
+        <th class="text-center">
           Name
         </th>
-        <th class="text-left">
+        <th class="text-center">
           Access level
         </th>
-        <th class="text-left">
+        <th class="text-center">
           Email
         </th>
       </tr>
     </thead>
     <tbody>
       <tr
+        class=" bg-zinc-50 hover:bg-zinc-400"
         v-for="item in users"
         :key="item.name"
       >
-        <td>{{ item.fullname }}</td>
-        <td>{{ item.access_level }}</td>
-        <td>{{ item.email }}</td>
+        <td class="px-0">
+          <router-link
+            :to="{ name: 'index' }"
+            class="w-full h-full"
+          >
+
+          <div v-if="item?.fullname" class="w-[45px] h-full flex gap-2 items-center pl-4">
+            {{ item.fullname }}
+          </div>
+          </router-link>
+        </td>
+
+        <td class="px-0">
+          <router-link
+            :to="{ name: 'index' }"
+            class="w-full h-full"
+          >
+          <div v-if="item?.access_level" class="w-[45px] h-full flex gap-2 items-center pl-4">
+            {{ item.access_level }}
+          </div>
+          </router-link>
+        </td>
+
+        <td class="px-0">
+          <router-link
+            :to="{ name: 'index' }"
+            class="w-full h-full"
+          >
+          <div v-if="item?.email" class="w-[45px] h-full flex gap-2 items-center pl-4">
+            {{ item.email }}
+          </div>
+          </router-link>
+        </td>
 
       </tr>
     </tbody>
