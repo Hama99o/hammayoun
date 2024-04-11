9926ae0fde4a7c315a293c1c13702d376b0bdc75
hama <hmmshl@gmail.com>
2024-04-11 17:22:27 +0200

    naming fixed

--- app/models/note_app/note.rb
+++ app/models/note_app/note.rb
@@ -1,2 +1,18 @@
-class TagApp::Tag < ApplicationRecord
+# == Schema Information
+#
+# Table name: note_app_notes
+#
+#  id          :bigint           not null, primary key
+#  owner_id    :bigint
+#  data        :jsonb
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_note_app_notes_on_owner_id  (owner_id)
+#
+class NoteApp::Note < ApplicationRecord
 end
--- app/models/note_app/share.rb
+++ app/models/note_app/share.rb
@@ -1,2 +1,17 @@
-class TagApp::Share < ApplicationRecord
+# == Schema Information
+#
+# Table name: note_app_shares
+#
+#  id                  :bigint           not null, primary key
+#  note_id             :bigint
+#  shared_with_user_id :bigint
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_note_app_shares_on_note_id              (note_id)
+#  index_note_app_shares_on_shared_with_user_id  (shared_with_user_id)
+#
+class NoteApp::Share < ApplicationRecord
 end
