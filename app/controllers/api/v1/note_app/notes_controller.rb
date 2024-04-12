2788b4151aa851a31c3e3ab689093487641583f0
hama <hmmshl@gmail.com>
2024-04-12 22:35:43 +0200

    print removed

--- app/controllers/api/v1/note_app/notes_controller.rb
+++ app/controllers/api/v1/note_app/notes_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::NoteApp::NotesController < ApplicationController
 
   def index
-    p current_user
-    p 'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii'
     paginate_render(
       NoteApp::NoteSerializer,
       policy_scope(current_user.notes.where(status: :published).order(created_at: :desc)))
