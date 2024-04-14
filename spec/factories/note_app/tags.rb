7ad3a1edbb65249424ae61ed30f9e47168a273b3
hama <hmmshl@gmail.com>
2024-04-14 12:42:37 +0200

    NoteApp::Tag added

---
+++ spec/factories/note_app/tags.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+FactoryBot.define do
+  factory :note_tag, class: "NoteApp::Tag" do
+    name { Faker::Book.title }
+  end
+end
