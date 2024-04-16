458c550b3ef04f5077a5b2586a98ef964478a05e
hama <hmmshl@gmail.com>
2024-04-16 23:48:31 +0200

    trashes added but not done

---
+++ app/javascript/components/note_app/notes/NoteTrashes.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-dialog max-width="500" v-model="isActive">
+      <template #default>
+
+        <div class="w-full flex flex-col p-6 gap-8 bg-white">
+          <!-- Icon and text -->
+          <div class="flex flex-col items-center gap-4">
+            <!-- icon -->
+            <v-icon
+              icon="mdi mdi-trash-can-outline"
+              height="52"
+              width="52"
+            />
+
+            <!-- Title & description -->
+            <div class="flex flex-col items-center self-stretch gap-4">
+              <p class="text-fakeBlack text-xl font-medium text-center">
+                Trashes
+              </p>
+            </div>
+          </div>
+
+
+          <!-- Buttons -->
+          <div class="flex flex-col items-start gap-3 self-stretch lg:!flex-row">
+            <v-btn
+              variant="outlined"
+              color="fakeBlack"
+              class="normal-case text-xs w-full lg:!flex-1"
+              text="cancel"
+              @click="isActive = false"
+            />
+
+            <v-btn
+              variant="flat"
+              color="negativeRed"
+              class="normal-case text-xs w-full lg:!w-1/2"
+              text="Add User"
+              @click="emit('add-user', role, email, 'add')"
+            />
+          </div>
+        </div>
+      </template>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+const role = ref('')
+const email = ref('')
+const isActive = ref(false)
+const emit = defineEmits(['add-user'])
+
+defineExpose({
+  isActive
+})
+</script>
--- app/javascript/views/note_app/notes/Index.vue
+++ app/javascript/views/note_app/notes/Index.vue
@@ -5,17 +5,22 @@
       <h1>Notes</h1>
       <v-icon @click="createNewNote" icon="mdi mdi-plus  hover:!bg-very-light-grey " class="cursor-pointer create-note-btn"/>
     </div>
-    <v-text-field
-      v-model="search"
-      color="primary"
-      class="w-full text-darkGrey md:w-[258px]"
-      variant="outlined"
-      density="compact"
-      hide-details
-      prepend-inner-icon="mdi-magnify"
-      :label="'Search'"
-      @update:model-value="searchNote"
+    <div class="flex ">
+      <v-text-field
+        v-model="search"
+        color="primary"
+        class="w-full text-darkGrey md:w-[258px]"
+        variant="outlined"
+        density="compact"
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        :label="'Search'"
+        @update:model-value="searchNote"
       />
+      <v-btn class="bg-red-500 hover:bg-red-100 ml-4" dark @click="handleClick">
+        <span class="mdi mdi-trash-can-outline"></span>
+      </v-btn>
+    </div>
   </div>
 
   <note-table :notes="notes" />
@@ -33,7 +38,10 @@
   <open-note
     ref="isNoteOpened"
     :note="selectedNote"
-    @add-user="inviteUserWithEmail"
+  />
+
+  <note-trashes
+    ref="isNoteTrashesDialogOpened"
   />
 </template>
 
@@ -43,19 +51,22 @@ import { storeToRefs } from 'pinia';
 import { useNoteStore } from '@/stores/note_app/note.store';
 import { onMounted, ref } from 'vue';
 import OpenNote from '@/components/note_app/notes/OpenNote.vue';
+import NoteTrashes from '@/components/note_app/notes/NoteTrashes.vue';
 
-const { fetchNotes, resetStates, createNote } = useNoteStore();
+const { fetchNotes, fetchTrashesNotes, resetStates, createNote } = useNoteStore();
 
 const { notes, pagination, page, search } = storeToRefs(useNoteStore());
 
 
 const selectedNote = ref(null)
 const isNoteOpened = ref(null)
+const isNoteTrashesDialogOpened = ref(null)
 
 onMounted(async () => {
   try {
     await resetStates()
     await fetchNotes();
+    await fetchTrashesNotes();
   } catch (error) {
     console.log(error);
   }
@@ -65,6 +76,11 @@ const openNoteDialog = (note) => {
   selectedNote.value = note
   isNoteOpened.value.isOpen = true
 }
+
+const handleClick = (note) => {
+  isNoteTrashesDialogOpened.value.isActive = true
+}
+
 
 const fetchNewPage = async(e) => {
   try {
