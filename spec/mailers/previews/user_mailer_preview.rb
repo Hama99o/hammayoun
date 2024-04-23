7914c93689557b65f111d59f7c3d0c42d1166a33
hama <hmmshl@gmail.com>
2024-04-23 11:55:28 +0200

    email fixed

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
-    UserMailer.welcome_email(User.find_by(email: 'hmmshll@gmail.com'))
+    UserMailer.welcome_email(User.find_by(email: 'hmmshl@gmail.com'))
   end
 end
