3d9bfe118383c0856a4212771ac6ddf70e6b6a08
hama <hmmshl@gmail.com>
2024-04-24 10:46:18 +0200

    create method added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    super do |resource|
+      if resource.persisted?
+        # Call mailer to send welcome email
+        UserMailer.welcome_email(resource).deliver_now
+      end
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
